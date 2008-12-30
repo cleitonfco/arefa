4f0c9ae77aca11c0d1ec41f938ac5c6dfef0ecaf
Cleiton Francisco <cleitonfco@gmail.com>
2008-12-30 11:00:40 -0300

    Project of Task can't be changed

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,9 @@
 class Task < ActiveRecord::Base
-  validates_presence_of :description
+  validates_presence_of :description, :project_id
   validates_inclusion_of :priority, :in => 0..2
+
+  def validate_on_update
+    errors.add(:project_id, I18n.t("task.errors.project")) if ( self.project_id != @project_id ) 
+  end
+
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -21,6 +21,19 @@ describe Task do
     task.should have(1).error_on(:description)
   end
 
+  it "should valid presence of project" do
+    task = Task.new
+    task.should_not be_valid
+    task.should have(1).error_on(:project_id)
+  end
+
+  it "should not change project" do
+    task = Task.create!(@valid_attributes)
+    task.project_id = "2"
+    task.should_not be_valid
+    task.should have(1).error_on(:project_id)
+  end
+
   it "should not be a priority below the acceptable" do
     task = Task.new
     task.priority = -1
@@ -32,6 +45,29 @@ describe Task do
     task = Task.new
     task.priority = 3
     task.should have(1).error_on(:priority)
+  end
+
+  it "should validate when saving" do
+    task = Task.new
+    task.save.should be_false
+  end
+
+  it "should validate when updating attributes" do
+    task = Task.new
+    task.update_attributes({}).should be_false
+    task.update_attributes(@valid_attributes).should be_true
+  end
+
+  it "should find by id" do
+    task = Task.create!(@valid_attributes)
+    Task.find(task.id).should == task
+  end
+
+  it "should destroy by id" do
+    task = Task.create!(@valid_attributes)
+    lambda {
+      Task.destroy(task.id)
+    }.should change(Task, :count).by(-1)
   end
 
 end
